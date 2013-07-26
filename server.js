fc3a4edff309fbb8322936a478ff3c51cb6748a7
ichenkun <ichenkun@gmail.com>
2013-07-26 11:04:36 +0800

    update config

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var config = require('./config');
 var Controller = require('./server/controller');
 var MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect('mongodb://127.0.0.1:'+ config.dbport +'/' + config.dbname, function(err, db){
+MongoClient.connect('mongodb://127.0.0.1:'+ config.dbPort +'/' + config.dbName, function(err, db){
 	db.admin().authenticate(config.adminUsername, config.adminPassword, function(err, reslut){
 		if (!err) {
 			initSuper(db);
@@ -23,10 +23,10 @@ function initSuper(db) {
 	db.collection('users').count({ role: 'super' }, function(err, count){
 		if (count == 0) {
 			var shaSum = crypto.createHash('sha256');
-			shaSum.update(config.password);
+			shaSum.update(config.superPassword);
 
 			db.collection('users').insert({
-				username : config.username,
+				username : config.superUsername,
 				password: shaSum.digest('hex'),
 				role: config.role,
 				create_at: Date.now(),
